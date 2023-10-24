ac023f6cb52d1b920edec183812ba0b5eb487c64
Jakub Piwko <jakub.piwko@op.pl>
2023-10-24 23:41:50 +0200

    BST-TREE Class and Basic Menu

---
+++ class1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Klasa Tree
+
+class Tree {
+public:
+    int data;
+    Tree* left;
+    Tree* right;
+
+    // Konstruktor
+    Tree(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+int main() {
+    
+    
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Program Drzewo BST" << endl;
+    cout << "Wybierz opcje: " << endl;
+    cout << "1. Dodaj element" << endl;
+    cout << "2. Usun element" << endl;
+    cout << "3. Usun cale drzewo" << endl;
+    cout << "4. Szukaj drogi do podanego elementu" << endl;
+    cout << "5. Wyswietl drzewo" << endl;
+    cout << "6. Zapis do pliku" << endl;
+    cout << "7. Odczyt z pliku" << endl;
+
+    return 0;
+}
