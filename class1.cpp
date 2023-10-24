211c446509d760b31470698638546bbe39499ce8
Jakub Piwko <jakub.piwko@op.pl>
2023-10-25 00:27:36 +0200

    BST-TREE | Delete Tree

--- class1.cpp
+++ class1.cpp
@@ -14,6 +14,7 @@ public:
 
 };
 
+
     // Dodawanie elementu do drzewa BST
     void insertNode(Tree*& root, int value) {
     if (root == nullptr) {
@@ -26,6 +27,7 @@ public:
         }
     }
     }
+
 
     // Wyświetlenie drzewa BST przy użyciu InOrder
     void printInOrder(Tree* root) {
@@ -44,11 +46,29 @@ public:
     }
 
 
+    // Usuwanie drzewa BST przy użyciu deleteTree || Problem z dodawaniem elementu po usunięciu drzewa
+    void deleteTree(Tree* root) {
+    if (root == nullptr) {
+        return;
+    }
+
+    // Najpierw usuń lewe poddrzewo
+    deleteTree(root->left);
+
+    // Następnie usuń prawe poddrzewo
+    deleteTree(root->right);
+
+    // Ustawienie węzła na pusty
+    delete root;
+    root = nullptr;   
+}
+
 int main() {
     
     //Tree* root = nullptr; // Inicjalizacja pustego drzewa
     //insertNode(root, 50);
     //printInOrder(root);
-
+    //deleteTree(root);
+    
     return 0;
 }
